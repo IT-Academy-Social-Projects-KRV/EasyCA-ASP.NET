a68c921b3a1ce0911e311a11c5988718f8d8d2be
pinkevycdima@gmail.com <pinkevycdima@gmail.com>
2021-08-16 13:31:37 +0300

    create project

---
+++ AccountService.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountService.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ AccountService.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountService.Domain
+{
+    public class Class1
+    {
+    }
+}
