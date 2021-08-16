7b4a68666a5d8efc343961362660320a09b7397e
Denis Matiushko <d.matiushko@ukr.net>
2021-08-16 15:02:34 +0300

    Task #23 - Fixing unnecessary using directives

--- AccountService.Data/Entities/DriverLicense.cs
+++ AccountService.Data/Entities/DriverLicense.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AccountService.Data.Entities
+﻿namespace AccountService.Data.Entities
 {
     public class DriverLicense
     {
