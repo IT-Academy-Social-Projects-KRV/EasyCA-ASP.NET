b9082b93e0e6c02803def3e82f404edcb50e99dd
pinkevycdima@gmail.com <pinkevycdima@gmail.com>
2021-08-17 14:08:29 +0300

    fix dependency in Easy.Data

--- AccountService.Data/Entities/User.cs
+++ AccountService.Data/Entities/User.cs
@@ -8,7 +8,6 @@
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public PersonalData UserData { get; set; }
-        
         }
     }
     
