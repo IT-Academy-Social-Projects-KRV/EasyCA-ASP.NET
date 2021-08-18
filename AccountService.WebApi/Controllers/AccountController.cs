d22b1244a7ed0e752d4426baef82d30dec844017
KiseRyota1488 <shavaryn_ak19@nuwm.edu.ua>
2021-08-18 22:44:54 +0300

    Created account controller, added exception in service account

---
+++ AccountService.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using AccountService.Domain.Interfaces;
+using AccountService.Domain.RequestModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AccountService.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IServiceAccount _serviceAccount;
+        public AccountController(IServiceAccount serviceAccount)
+        {
+            _serviceAccount = serviceAccount;
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserRegisterRequest userRegisterRequest)
+        {
+            await _serviceAccount.RegisterUser(userRegisterRequest);
+            return Ok();
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginRequest userLoginRequest)
+        {
+            await _serviceAccount.LoginUser(userLoginRequest);
+            return Ok();
+        }
+    }
+}
