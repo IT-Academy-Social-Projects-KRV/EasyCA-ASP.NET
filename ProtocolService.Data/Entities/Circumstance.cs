1d71ccf9b661e856ddeb2371486879ad1195c79d
Denis Matiushko <d.matiushko@ukr.net>
2021-09-12 14:20:37 +0300

    #88 Create Seed for Circumstances

---
+++ ProtocolService.Data/Entities/Circumstance.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace ProtocolService.Data.Entities
+{
+    public class Circumstance
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CircumstanceId { get; set; }
+        [BsonElement("Circumstances Name")]
+        [JsonProperty("Circumstances Name")]
+        public string CircumstanceName { get; set; }
+    }
+}
