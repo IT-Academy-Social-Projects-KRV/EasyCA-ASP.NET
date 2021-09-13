048332b1419a8f15df5c3d488dbb1934ee82581a
Ivan Kosmin <kosminfeed@gmail.com>
2021-09-13 19:02:00 +0300

    Add db service to protocol service web api

--- ProtocolService.WebApi/Startup.cs
+++ ProtocolService.WebApi/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using ProtocolService.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +15,18 @@ namespace ProtocolService.WebApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IMongoClient>(s => new MongoClient(Configuration.GetConnectionString("MongoDb")));
+            services.AddScoped(s => new ProtocolDbContext(s.GetRequiredService<IMongoClient>(), Configuration["DbName"]));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
