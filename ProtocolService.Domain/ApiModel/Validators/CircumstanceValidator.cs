2fe2c3132aa6ad673cdc0e8257023f61b2c61611
Kost_ni27@student.itstep.org <Kost_ni27@student.itstep.org>
2021-09-22 14:00:43 +0300

    Create AddreddOfAccidentValidator, CircumstanceValidator, SideValidator and WitnessValidator

---
+++ ProtocolService.Domain/ApiModel/Validators/CircumstanceValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using ProtocolService.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProtocolService.Domain.ApiModel.Validators
+{
+    public class CircumstanceValidator : AbstractValidator<Circumstance>
+    {
+        public CircumstanceValidator()
+        {
+            RuleFor(n => n.CircumstanceName).Cascade(CascadeMode.Stop).NotEmpty().WithMessage("Circumstance Name is required").
+               NotNull().WithMessage("Circumstance Name could not be null").Must(IsValidCircumstanceName).
+               WithMessage("Something wrong with circumstance name");
+        }
+
+        public static bool IsValidCircumstanceName(string circumstanceName)
+        {
+            string circumstanceNamePattern = @"^[А-ЩЬЮЯЇІЄҐA-Z]{1}[а-щьюяїієґa-z]{70}$"; //уточнити чи 70 символів досить
+
+            if (Regex.IsMatch(circumstanceName, circumstanceNamePattern))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProtocolService.Domain/ApiModel/Validators/SideValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using ProtocolService.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProtocolService.Domain.ApiModel.Validators
+{
+    public class SideValidator : AbstractValidator<Side>
+    {
+        public SideValidator()
+        {
+            RuleFor(n => n.DriverLicenseSerial).Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("Driver license serial is required")
+                .Must(IsValidDriverLicenseSerial).WithMessage("Driver license serial format is not valid");
+        }
+
+        public static bool IsValidDriverLicenseSerial(string driverLicenseSerial)
+        {
+            string driverLicenseSerialPattern = @"[А-ЩЬЮЯЇІЄҐA-Z]{3}[0-9]{6}$";
+            Regex driverLicenseSerialValidation = new Regex(driverLicenseSerialPattern);
+
+            if (driverLicenseSerial == null) return false;
+
+            if (driverLicenseSerialValidation.IsMatch(driverLicenseSerial))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Error in PHN");
+                return false;
+            }
+        }
+    }
+}
