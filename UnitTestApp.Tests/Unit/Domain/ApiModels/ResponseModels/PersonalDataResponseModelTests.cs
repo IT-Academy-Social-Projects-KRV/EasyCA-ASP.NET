4ba248e203a0ee21841a999bec69b3a552f70510
pinkevycdima@gmail.com <pinkevycdima@gmail.com>
2021-09-23 14:14:46 +0300

    add unitTest

---
+++ UnitTestApp.Tests/Unit/Domain/ApiModels/ResponseModels/PersonalDataResponseModelTests.cs
@@ -0,0 +1,32 @@
+﻿using CrudMicroservice.Data.Entities;
+using CrudMicroservice.Domain.ApiModel.ResponseApiModels;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTestApp.Tests.Unit.Domain.ApiModels.ResponseModels
+{
+    public class PersonalDataResponseModelTests
+    {
+        [Theory]
+        [InlineData(null, "12341234", "123123123", "03.10.2002", "programmer",null,null)]
+        public void Ctor_ShouldImplementParameters(Address address, string ipn, 
+            string serviceNumber, DateTime birthDay, 
+            string jobPosition, DriverLicense userDriverLicense, 
+            List<string> userCars)
+        {
+            // Act
+
+            var testObject = new PersonalDataResponseModel();
+            //Assert
+
+            Assert.Equal(address, testObject.Address);
+            Assert.Equal(ipn, testObject.IPN);
+            Assert.Equal(serviceNumber, testObject.ServiceNumber);
+            Assert.Equal(birthDay, testObject.BirthDay);
+            Assert.Equal(jobPosition, testObject.JobPosition);
+            Assert.Equal(userDriverLicense, testObject.UserDriverLicense);
+            Assert.Equal(userCars, testObject.UserCars);
+        }
+    }
+}
---
+++ UnitTestApp.Tests/Unit/Domain/ApiModels/ResponseModels/UserResponseModels.cs
@@ -0,0 +1,28 @@
+﻿using CrudMicroservice.Domain.ApiModel.ResponseApiModels;
+using Xunit;
+
+namespace UnitTestApp.Tests
+{
+    public class UserResponseModelTests
+    {
+        [Theory]
+        [InlineData("Email", "FirstName", "LastName")]
+        [InlineData(null, "Null", "0")]
+        [InlineData("", "", "")]
+        public void Ctor_ShouldImplementParameters(string Email, string FirstName, string LastName)
+        {
+            //Act
+            var actualApiModel = new UserResponseModel
+            {
+                Email = Email,
+                FirstName = FirstName,
+                LastName = LastName
+            };
+
+            //Assert
+            Assert.Equal(Email, actualApiModel.Email);
+            Assert.Equal(FirstName, actualApiModel.FirstName);
+            Assert.Equal(LastName, actualApiModel.LastName);
+        }
+    }
+}
