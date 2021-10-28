310e4139dca9bb041cd61cc0fcd4e1d0392f770c
Andrii Shybko <aliquispassim@gmail.com>
2021-10-28 11:30:59 +0300

    Remove unused references

--- UnitTestApp.Tests/Unit/Domain/Services/AuthServiceUnitTest.cs
+++ UnitTestApp.Tests/Unit/Domain/Services/AuthServiceUnitTest.cs
@@ -8,16 +8,11 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Security.Claims;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
