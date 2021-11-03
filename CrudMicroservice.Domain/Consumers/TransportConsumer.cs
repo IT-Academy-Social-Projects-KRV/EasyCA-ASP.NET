90deabd1e81d71933509382b3a493db0bf315bb8
SashaKorniichuk <SashaKorniichuk2@gmail.com>
2021-11-03 12:52:52 +0200

    add exception if data is not valid

--- CrudMicroservice.Domain/Consumers/TransportConsumer.cs
+++ CrudMicroservice.Domain/Consumers/TransportConsumer.cs
@@ -31,27 +31,20 @@ namespace CrudMicroservice.Domain.Consumers
             Regex regVin = new Regex(@"^(([a-h,A-H,j-n,J-N,p-z,P-Z,0-9]{9})([a-h,A-H,j-n,J-N,p,P,r-t,R-T,v-z,V-Z,0-9])([a-h,A-H,j-n,J-N,p-z,P-Z,0-9])(\d{6}))$");
             Regex regCarPlate = new Regex(@"^[АВСРІ]{1}[АIВСЕНКМТРХОЄ]{1}\d{4}[А-Я]{2}$");
 
+            TransportResponseModelRabbitMQ responseTransport = new TransportResponseModelRabbitMQ();
+
             if (regVin.IsMatch(filter))
             {
 
                 transport = await _transportRepository.GetByFilterAsync(x => x.VINCode == filter);
+                responseTransport = _mapper.Map<TransportResponseModelRabbitMQ>(transport);
             }
             else if (regCarPlate.IsMatch(filter))
             {
                 transport = await _transportRepository.GetByFilterAsync(x => x.CarPlate == filter);
+                responseTransport = _mapper.Map<TransportResponseModelRabbitMQ>(transport);
             }
-            else
-            {
-                throw new RestException(HttpStatusCode.BadRequest, Resources.ResourceManager.GetString("WrongData"));
-            }
-
-            var responseTransport = _mapper.Map<TransportResponseModelRabbitMQ>(transport);
-
-            if (responseTransport == null)
-            {
-                throw new RestException(HttpStatusCode.BadRequest, Resources.ResourceManager.GetString("TransportClear"));
-            }
-
+         
             await context.RespondAsync(responseTransport);
         }
     }
